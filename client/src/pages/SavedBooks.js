cd77439527c4c38ba68b2d94baf545fcfe2f7f0e
chl850405 <christimarchetti@gmail.com>
2022-01-24 16:42:55 -0500

    heroku

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,51 +1,49 @@
-import React, { useState} from 'react';
-import {useQuery, useMutation} from '@apollo/react-hooks';
-import {GET_ME} from '../queries';
-import {REMOVE_BOOK} from '../utils/mutations';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { removeBookId } from '../utils/localStorage';
-
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+} from "react-bootstrap";
+import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
-  const {loading, data} = useQuery(GET_ME);
+  const { loading, data } = useQuery(GET_ME);
 
-  const[userData, setData] = useState(loading ? null : data.me);
+  const [userData, setData] = useState(loading ? null : data.me);
   // const newdata = {...userData?.me};
   const [removeBook] = useMutation(REMOVE_BOOK);
 
-  // useEffect(() =>{
-  //   if(userData){
-  //     console.log('useeffect!!')
-  //   }
-  // },[userData])
-  
-  if(!userData){
-      return null
+  if (!userData) {
+    return null;
   }
 
   const handleDeleteBook = async (bookId) => {
     try {
-       const data = await removeBook({
-        variables: {bookId},
+      const data = await removeBook({
+        variables: { bookId },
       });
 
       // update state of books:
-      setData(()=>{
-        return{
+      setData(() => {
+        return {
           ...userData,
-          savedBooks: data.data.removeBook.savedBooks
-        }
-      })
+          savedBooks: data.data.removeBook.savedBooks,
+        };
+      });
     } catch (err) {
       console.error(err);
     }
     removeBookId(bookId);
   };
 
-
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <Jumbotron fluid className="text-light bg-dark">
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
@@ -53,27 +51,37 @@ const SavedBooks = () => {
       <Container>
         <h2>
           {!loading && userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'
-          }
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? "book" : "books"
+              }:`
+            : "You have no saved books!"}
         </h2>
         <CardColumns>
-          {!loading && userData.savedBooks.map((book) => {
+          {!loading &&
+            userData.savedBooks.map((book) => {
               return (
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                <Card key={book.bookId} border="dark">
+                  {book.image ? (
+                    <Card.Img
+                      src={book.image}
+                      alt={`The cover for ${book.title}`}
+                      variant="top"
+                    />
+                  ) : null}
                   <Card.Body>
                     <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
+                    <p className="small">Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                    <Button
+                      className="btn-block btn-danger"
+                      onClick={() => handleDeleteBook(book.bookId)}
+                    >
                       Delete this Book!
                     </Button>
                   </Card.Body>
                 </Card>
               );
-            })
-          }
+            })}
         </CardColumns>
       </Container>
     </>
